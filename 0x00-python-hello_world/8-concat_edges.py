6beb55b0967ee2e53088747ffc539497295682eb
Itsrhoda” <rodahmanyaga.com>
2022-11-28 10:42:27 -0800

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted , interactive, object-oriented programming\
-        language that combine remrkable power with very vlear syntax"
+    language that combine remrkable power with very vlear syntax"
 str = str[39:66] + str[106:112] + str[:6]
 print(str)
