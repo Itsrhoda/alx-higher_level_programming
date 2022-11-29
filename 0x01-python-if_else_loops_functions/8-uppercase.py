5e68fae35bb756e150089a84cf41b03c9f9994f9
Itsrhoda” <rodahmanyaga.com>
2022-11-29 11:03:06 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i])< 123:
-            leter = 32
-        else:
-            letter = 0
-            print("{:c}".format(ord(str[i]) - letter), end='')
-    print()
+uppercase = __import__('8-uppercase').uppercase
+
+uppercase("holberton")
+uppercase("Holberton School98 Battery street")
