b5a9a05fd90ee5061a9bc37d5e5fd2817fd4664e
Itsrhoda” <rodahmanyaga.com>
2022-11-29 01:58:00 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!~/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord (str[i])< 123:
+        if ord(str[i]) >= 97 and ord(str[i])< 123:
             leter = 32
         else:
             letter = 0
