012e28c053a752077d8fdf2b8ae8fd5faf76a543
Itsrhoda” <rodahmanyaga.com>
2022-12-05 07:13:40 -0800

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,10 @@
 #include <Python.h>
 #include <object.h>
 #include <listobject.h>
+/**
+ * print_python_list_info - prints some basic info about Python lists.
+ * @p: python object
+ */
 
 void print_python_list_info(PyObject *p)
 {
