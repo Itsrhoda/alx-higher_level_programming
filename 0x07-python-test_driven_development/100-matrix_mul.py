116a6712e5d6930bb499f6a3f27fa7075ad7cbcc
Itsrhoda” <rodahmanyaga.com>
2023-01-06 04:04:46 -0800

    100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -6,12 +6,12 @@
 def matrix_mul(m_a, m_b):
     """Multiply two matrices.
     Args:
-        m_a (list of lists of ints/floats): The first matrix
+        m_a (list of lists of ints/floats): The first matrix.
         m_b (list of lists of ints/floats): The second matrix.
     Raises:
         TypeError: If either m_a or m_b is not a list of lists of ints/floats.
         TypeError: If either m_a or m_b is empty.
-        TypeError: If either m_a or m_b has diffrent-sized rows.
+        TypeError: If either m_a or m_b has different-sized rows.
         ValueError: If m_a and m_b cannot be multiplied.
     Returns:
         A new matrix representing the multiplication of m_a by m_b.
@@ -27,7 +27,7 @@ def matrix_mul(m_a, m_b):
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
 
-    if not all(isinstance(row, list) for now in m_a):
+    if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
