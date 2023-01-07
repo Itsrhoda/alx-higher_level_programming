b0c8ee5bd850b2dc0d5d37cde347e8221f8187cc
Itsrhoda” <rodahmanyaga.com>
2023-01-07 04:02:17 -0800

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,6 +5,7 @@
 
 def matrix_divided(matrix, div):
     """Divide all elements of a matrix.
+
     Args:
         matrix (list): A list of lists of ints or floats.
         div (int/float): The divisor.
@@ -17,12 +18,12 @@ def matrix_divided(matrix, div):
         A new matrix representing the result of the division.
     """
     if (not isinstance(matrix, list) or matrix == [] or
-            not all(isinstance(row, list) for now in matrix) or
+            not all(isinstance(row, list) for row in matrix) or
             not all((isinstance(ele, int) or isinstance(ele, float))
                     for ele in [num for row in matrix for num in row])):
         raise TypeError("matrix must be a matrix (list of lists) of "
                         "integers/floats")
-
+    
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
