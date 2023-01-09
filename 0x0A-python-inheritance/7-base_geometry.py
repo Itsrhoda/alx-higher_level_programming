5c4cef3271b7e674274db94c5ec63e58552c920d
Itsrhoda” <rodahmanyaga.com>
2023-01-09 05:03:18 -0800

    7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,7 +7,7 @@ class BaseGeometry:
 
     def area(self):
         """Not yet implemented."""
-        rise Exception("area() is not implemented")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """Validate a parameter as an integer.
